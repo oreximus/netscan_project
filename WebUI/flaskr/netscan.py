067dd8fa1e35716bc29287e49569f0c9f9826bb4
oreximus <oreximus>
2023-12-21 18:22:19 +0530

    Upadating and Managing the Codes

--- WebUI/flaskr/netscan.py
+++ WebUI/flaskr/netscan.py
@@ -12,8 +12,44 @@ from io import StringIO
 import sys
 
 
-bp = Blueprint('blog', __name__)
+# Defined Functions needed in the code!
+# Fetches the scan information by their
+# id and return the value of existed
+# scan information as scan to the program
 
+def get_scan(id, check_scan=True):
+    scan = get_db().execute(
+        'SELECT s.id, ip_address, description, created, scan_id, username'
+        ' FROM scan s JOIN user u ON s.scan_id = u.id'
+        ' WHERE s.id = ?',
+        (id,)
+    ).fetchone()
+
+    if scan is None:
+        abort(404, f"Scan id {id} doesn't exist.")
+
+    if check_scan and scan['scan_id'] != g.user['id']:
+        abort(403)
+
+    return scan
+
+
+# The Blueprints are used in the place
+# route and is placed in the code in
+# order where we can access the function
+# Which are needed!
+
+# Declaration of the netscan Application
+# Blueprint so we can acces this from
+# our __init__.py
+
+bp = Blueprint('netscan', __name__)
+
+
+# Cotain function to fetch all the Desired Logs
+# Information Associated with the Logged-In user
+# in a Logged-In Session Dashboard which is the
+# Index page.
 
 @bp.route('/')
 def index():
@@ -27,8 +63,14 @@ def index():
         (user_id,)
     ).fetchall()
 
-    return render_template('blog/index.html', scans=scans)
+    return render_template('netscan/index.html', scans=scans)
 
+
+# Scanning Function which uses nmap3 library for
+# Ip Scanning with vulners and mincvss+5.0 script
+# in the arguments and it dumps all the results
+# in json format with print standard output and get
+# stored in the database
 
 @bp.route('/<int:id>/index', methods=('POST',))
 @login_required
@@ -54,8 +96,12 @@ def scanning(id):
     db.execute("UPDATE scan SET scan_data = ? WHERE id = ?",
                (final_data, id,))
     db.commit()
-    return redirect(url_for('blog.index'))
+    return redirect(url_for('netscan.index'))
 
+
+# Create Function used to add the IP with their
+# Description and Store their Information in the
+# Database
 
 @bp.route('/', methods=('GET', 'POST'))
 @login_required
@@ -78,27 +124,14 @@ def create():
                 (ip_addr, description, g.user['id'])
             )
             db.commit()
-            return redirect(url_for('blog.index'))
+            return redirect(url_for('netscan.index'))
 
-    return render_template('blog/index.html')
+    return render_template('netscan/index.html')
 
 
-def get_scan(id, check_scan=True):
-    scan = get_db().execute(
-        'SELECT s.id, ip_address, description, created, scan_id, username'
-        ' FROM scan s JOIN user u ON s.scan_id = u.id'
-        ' WHERE s.id = ?',
-        (id,)
-    ).fetchone()
-
-    if scan is None:
-        abort(404, f"Scan id {id} doesn't exist.")
-
-    if check_scan and scan['scan_id'] != g.user['id']:
-        abort(403)
-
-    return scan
-
+# Function to Update IP and their Description
+# Information and Store the Modified Data in
+# the Database
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
@@ -123,10 +156,13 @@ def update(id):
                 (ip_addr, description, id)
             )
             db.commit()
-            return redirect(url_for('blog.index'))
+            return redirect(url_for('netscan.index'))
 
-    return render_template('blog/update.html', scan=scan)
+    return render_template('netscan/update.html', scan=scan)
 
+
+# Function to Delete the Information of
+# IP and their Description from the Database
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
@@ -135,4 +171,4 @@ def delete(id):
     db = get_db()
     db.execute('DELETE FROM scan WHERE id = ?', (id,))
     db.commit()
-    return redirect(url_for('blog.index'))
+    return redirect(url_for('netscan.index'))
